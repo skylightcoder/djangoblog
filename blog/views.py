ffbd837a3eb830871bc2e810f660ceb0cbb23194
gokhan <gokhan.isik@tubitak.gov.tr>
2016-09-01 14:06:13 +0300

    template added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+def index(request):
+    data = {'mydata': "Hello world from my blog"}
+    return render(request,'blog/index.html', data)
 # Create your views here.
